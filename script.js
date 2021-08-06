67ceb47fbfed2fbeac8fc60e0e883e11b841e878
Modsia16 <84951325+Modsia16@users.noreply.github.com>
2021-08-05 23:41:37 -0700

    push up

--- script.js
+++ script.js
@@ -1,45 +1,103 @@
 var submitBtnEl = document.querySelector('.button')
 var inputValueEl = document.querySelector('.inputValue')
-var nameEl = document.querySelector('.name')
-var descEl = document.querySelector('.desc')
-var tempEl = document.querySelector('.temp')
 var displayEl = document.querySelector('.display')
 
-submitBtnEl.addEventListener('click', function(){
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValueEl.value+'&APPID=210755dcdd1accd679a3739fa23e78a4')
-    .then(response => response.json())
-    .then(data => {
-        useApiData(data)
-    })
 
-.catch (err => alert("Not a city"))
-
+submitBtnEl.addEventListener('click', ()=>{
+    console.log("button pressed") 
+    sendApiRequest()
 })
 
-/*function useApiData(data){
-document.querySelector('.display').innerHTML =`
-<div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">${data.main[0].name}</h5>
-    <p class="card-text">${data.weather[0].description}</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
+
+async function sendApiRequest() {
+let response = await fetch('https://api.openweathermap.org/data/2.5/forecast?q='+inputValueEl.value+'&appid=210755dcdd1accd679a3739fa23e78a4')
+console.log(response)    
+ let data = await response.json()
+        localStorage.setItem(inputValueEl)
+        useApiData(data)
+    }
+
+/*.catch (err => alert("Not a city")
+
+})*/
+
+function useApiData(data){
+document.querySelector('#display').innerHTML =`
+<div class="container">
+    <div class="row">
+        <div class="col-sm-3">
+            <div class="card-body border border-dark">
+                <h5 class="card-title">${data.city.name}</h5>
+                <p class="card-text">${data.list[0].dt_txt}</p>
+                <p class="card-text">Outside: ${data.list[0].weather[0].description}</p>
+                <p class="card-text">${data.list[0].weather[0].icon}</p>
+                <p class="card-text">Temperature: ${data.list[0].main.temp}</p>
+                <p class="card-text">Humidity: ${data.list[0].main.humidity}</p>
+                <p class="card-text">Wind Speed: ${data.list[0].wind.speed}</p>
+                <p class="card-text">UV Index: ${data.list[0].current}</p>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="card-body border border-dark">
+                <h5 class="card-title">${data.city.name}</h5>
+                <p class="card-text">${data.list[6].dt_txt}</p>
+                <p class="card-text">Outside: ${data.list[6].weather[0].description}</p>
+                <p class="card-text">${data.list[6].weather[0].icon}</p>
+                <p class="card-text">Temperature: ${data.list[6].main.temp}</p>
+                <p class="card-text">Humidity: ${data.list[6].main.humidity}</p>
+                <p class="card-text">Wind Speed: ${data.list[6].wind.speed}</p>
+                <p class="card-text">UV Index: ${data.list[6].current}</p>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="card-body border border-dark">
+                <h5 class="card-title">${data.city.name}</h5>
+                <p class="card-text">${data.list[13].dt_txt}</p>
+                <p class="card-text">Outside: ${data.list[13].weather[0].description}</p>
+                <p class="card-text">${data.list[13].weather[0].icon}</p>
+                <p class="card-text">Temperature: ${data.list[13].main.temp}</p>
+                <p class="card-text">Humidity: ${data.list[13].main.humidity}</p>
+                <p class="card-text">Wind Speed: ${data.list[13].wind.speed}</p>
+                <p class="card-text">UV Index: ${data.list[13].current}</p>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="card-body border border-dark">
+                <h5 class="card-title">${data.city.name}</h5>
+                <p class="card-text">${data.list[20].dt_txt}</p>
+                <p class="card-text">Outside: ${data.list[20].weather[0].description}</p>
+                <p class="card-text">${data.list[20].weather[0].icon}</p>
+                <p class="card-text">Temperature: ${data.list[20].main.temp}</p>
+                <p class="card-text">Humidity: ${data.list[20].main.humidity}</p>
+                <p class="card-text">Wind Speed: ${data.list[20].wind.speed}</p>
+                <p class="card-text">UV Index: ${data.list[20].current}</p>
+            </div>
+        </div>
+    </div>
 </div>
 `
-}*/
+}
 
+/*
+https://api.openweathermap.org/data/2.5/onecall?
+http://api.openweathermap.org/data/2.5/weather?
 /*var APIkey = "210755dcdd1accd679a3739fa23e78a4"
 var city;
 var state;*/
 
-
+        /*<div class="col-sm-3">
+            <div class="card-body border border-dark">
+                <h5 class="card-title">${data.city.name}</h5>
+                <p class="card-text">${data.list[27].dt_txt}</p>
+                <p class="card-text">outside: ${data.list[27].weather[0].description}</p>
+                <p class="card-text">${data.list[27].weather[0].icon}</p>
+                <p class="card-text">Temperature: ${data.list[27].main.temp}</p>
+                <p class="card-text">Humidity: ${data.list[27].main.humidity}</p>
+                <p class="card-text">Wind Speed: ${data.list[27].wind.speed}</p>
+                <p class="card-text">UV Index: ${data.list[27].current}</p>
+            </div>
+        </div>*/
 
 /*funciton getApi() {
     var requestURL = 'api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}'
 }*/
-
-
-
-  
-
-
